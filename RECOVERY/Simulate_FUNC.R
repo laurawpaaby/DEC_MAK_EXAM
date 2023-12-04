eb651687feeb21b5ba6dc8efc63f78d59e2a888f
Laura W. Paaby <laurapaaby18@gmail.com>
2023-12-04 19:56:56 +0100

    pushing simulations try

--- RECOVERY/Simulate_FUNC.R
+++ RECOVERY/Simulate_FUNC.R
@@ -1,27 +1,15 @@
-
-# TO DO 
-# - array structure 
-# - when to simulate what parameters 
-
-
-######## THE COMPUTER CONTRIBUTIONS OF TWO SUBJECTS - Ga
-trials <- array(1:12)
-s1_cont <- c(9,9,9,9,3,0,0,0,9,9,9,9)
-s2_cont <- c(9,6,6,6,0,0,0,0,3,0,0,0)
-matrix_con <- matrix(c(s1_cont,s2_cont), 12, 2)
-
-real_Ga <- rowMeans(matrix_con)
-
-
-
 #### Simulating data for parameter recovery ####
+
 hier_cc_sim <- function(ntrials, alpha, omega, rho){
   
-  # empty arrays to be filled 
-  #Gb <- array(NA,c(groupSize, ntrials,ngroups))
-  #Ga <- array(NA, c(groupSize, ntrials,ngroups))
-  #p <- array(NA,c(groupSize, ntrials,ngroups)) #preferences
-  #c <- array(NA,c(groupSize, ntrials,ngroups, 1)) #contribution
+  
+  ######## THE COMPUTER CONTRIBUTIONS OF TWO SUBJECTS - Ga
+  trials <- array(1:12)
+  s1_cont <- c(9,9,9,9,3,0,0,0,9,9,9,9)
+  s2_cont <- c(9,6,6,6,0,0,0,0,3,0,0,0)
+  matrix_con <- matrix(c(s1_cont,s2_cont), 12, 2)
+  
+  real_Ga <- rowMeans(matrix_con)
   
   ## empty array for simulated child 
   contri <- array(NA,12)
@@ -38,13 +26,13 @@ hier_cc_sim <- function(ntrials, alpha, omega, rho){
     if (t != 1){
       
       # calculate Gb (belief about group contribution)
-      Gb[t] <- ((1-omega)*(Gb[t-1]))+(omega*(Ga[t-1]))
+      Gb[t] <- ((1-omega)*(Gb[t-1]))+(omega*(real_Ga[t-1]))
       
       # calculate poisson parameter
       p <- rho[s]*Gb[t]
       
       # sample contribution from poisson
-      contri[t] <- dpois(p)
+      contri[t] <- dpois(p) ###  dictates the initial contribution of others (should be 0,3,6,9) => can either be manipulated somehow ?? or 0,1,2,3 and the rescaled. 
     }
     
     else {
