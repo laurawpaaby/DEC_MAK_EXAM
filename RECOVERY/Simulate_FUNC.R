52e63457aa46c1936c1dce13678a9b163e3d4dd9
Laura W. Paaby <laurapaaby18@gmail.com>
2023-12-06 15:46:34 +0100

    push simu try

--- RECOVERY/Simulate_FUNC.R
+++ RECOVERY/Simulate_FUNC.R
@@ -1,6 +1,6 @@
 #### Simulating data for parameter recovery ####
 
-hier_cc_sim <- function(ntrials, alpha, omega, rho){
+hier_cc_sim <- function(alpha, rho, omega){
   
   
   ######## THE COMPUTER CONTRIBUTIONS OF TWO SUBJECTS - Ga
@@ -18,7 +18,7 @@ hier_cc_sim <- function(ntrials, alpha, omega, rho){
   Gb <- array(NA, 12)
   
   ## Gb for first trial
-  Gb[1] <- rpois(alpha)
+  Gb[1] <- rpois(1,alpha)
   
   # simulating behavior for subject s in each trial
   for (t in 1:12) {
@@ -29,19 +29,19 @@ hier_cc_sim <- function(ntrials, alpha, omega, rho){
       Gb[t] <- ((1-omega)*(Gb[t-1]))+(omega*(real_Ga[t-1]))
       
       # calculate poisson parameter
-      p <- rho[s]*Gb[t]
+      p <- rho*Gb[t]
       
       # sample contribution from poisson
-      contri[t] <- dpois(p) ###  dictates the initial contribution of others (should be 0,3,6,9) => can either be manipulated somehow ?? or 0,1,2,3 and the rescaled. 
+      contri[t] <- rpois(1,p) ###  dictates the initial contribution of others (should be 0,3,6,9) => can either be manipulated somehow ?? or 0,1,2,3 and the rescaled. 
     }
     
     else {
       
       # calculate poisson parameter - first trial
-      p <- rho[s]*Gb[1]
+      p <- rho*Gb[1]
       
       # sample contribution from poisson - first trial
-      contri[t] <- dpois(p)
+      contri[t] <- rpois(1,p)
     }
     
   }
