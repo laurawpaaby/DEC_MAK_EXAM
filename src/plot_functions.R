faa202f74682937e5a1bbade1666ee03ed47a190
daniblu <mr.daniblu@gmail.com>
2024-01-01 21:18:43 +0100

    functions

---
+++ src/plot_functions.R
@@ -0,0 +1,133 @@
+label_parsed <- function(x) {
+  parsed_param <- gsub("alpha", "\U03B1", x)
+  parsed_param <- gsub("rho", "\U03C1", parsed_param)
+  parsed_param <- gsub("omega", "\U03C9", parsed_param)
+  
+  parsed_param
+  
+}
+
+
+
+label_parsed_delta <- function(x) {
+  parsed_param <- gsub("alpha", "\U03B1", x)
+  parsed_param <- gsub("rho", "\U03C1", parsed_param)
+  parsed_param <- gsub("omega", "\U03C9", parsed_param)
+  
+  parsed_param <- paste0("\U0394", parsed_param)
+  
+  parsed_param
+  
+}
+
+
+
+sim_sub_plot <- function(c){
+  
+  c_avg <- colMeans(c)
+  
+  c_sd <- apply(c, 2, sd)
+  
+  plot <- ggplot() +
+    geom_errorbar(aes(x = 1:12, ymin = c_avg - c_sd, ymax = c_avg + c_sd), width = 0.2) +
+    geom_point(aes(x = 1:12, y = c_avg), size=2, shape="square") +
+    geom_line(aes(x = 1:12, y = c_avg), lwd=0.3) +
+    theme_bw() +
+    theme(panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          strip.background = element_rect(fill = "transparent", colour = NA),
+          strip.text = element_text(size = 12, face = "bold")) +
+    ylab("Average contribution") +
+    xlab("Trial") +
+    ylim(-1, 3) +
+    scale_x_continuous(breaks = 1:12)
+
+  ggsave("plots/sim_sub_plot.png", width = 6, height = 4)
+  print("[INFO]: Saved plots/sim_sub_plot.png")
+}
+
+
+
+sub_recov_plot <- function(df){
+  
+  gg_l = lapply(dfs, function(x) {
+    
+    ggplot(x, aes(true_diff, recov_diff)) +
+      geom_point() +
+      geom_abline(intercept = 0, slope = 1, color = "black", lwd = 0.2) +
+      theme_bw() +
+      facet_wrap(~ parameter, ncol = 1, labeller = label_parsed) +
+      theme(strip.background = element_rect(fill = "transparent", colour = NA),
+            strip.text = element_text(size = 15, face = "bold"),
+            axis.title = element_text(size = 8),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank()) +
+      labs(x = "True", y = "Recovered")
+    
+  })
+  
+  plot <- wrap_plots(gg_l, ncol = 3, nrow = 1)
+  
+  # save
+  ggsave("plots/sub_recov.png", width = 12, height = 4.3)
+  print("[INFO]: Saved plots/sub_recov.png")
+}
+
+
+
+diff_recov_plot <- function(dfs) {
+  
+  gg_l = lapply(dfs, function(x) {
+    
+    ggplot(x, aes(true_diff, recov_diff)) +
+      geom_point(aes(color = true_mid)) +
+      geom_abline(intercept = 0, slope = 1, color = "black", lwd = 0.2) +
+      theme_bw() +
+      facet_wrap(~ parameter, ncol = 1, labeller = label_parsed_delta) +
+      theme(strip.background = element_rect(fill = "transparent", colour = NA),
+            strip.text = element_text(size = 15, face = "bold"),
+            axis.title = element_text(size = 8),
+            legend.position = c(0.78, 0.08),
+            legend.direction = "horizontal",
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank()) +
+      labs(color = "True mid-value", x = "True", y = "Recovered") +
+      scale_color_continuous(guide = guide_colorbar(title.position = "top",
+                                                    title.hjust = 0.5,
+                                                    title.vjust = 0.5,
+                                                    title.theme = element_text(size = 8),
+                                                    barwidth = 5,
+                                                    barheight = 0.5,
+                                                    label.theme = element_text(size = 7)))
+    
+  })
+  
+  plot <- wrap_plots(gg_l, ncol = 3, nrow = 1)
+  
+  ggsave("plots/diff_recov.png", width = 12, height = 4.5)
+  print("[INFO]: Saved plots/diff_recov.png")
+}
+
+
+
+group_diffs_plot <- function(df){
+  ggplot() +
+  geom_linerange(data = group_df_for_plot,
+                 aes(xmin = first, xmax = second, 
+                     y = comparison,
+                     color=sign)) +
+  ylab('') +
+  xlab('') +
+  theme_bw() +
+  facet_wrap(~parameter, ncol=3, scales="free_x") +
+  theme(panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        strip.background = element_rect(fill = "transparent", colour = NA),
+        strip.text = element_text(size = 12, face = "bold")) +
+  scale_y_discrete(breaks = NULL)
+  
+  ggsave("plots/group_diffs.png", width = 7, height = 3)
+  print("[INFO]: Saved plots/group_diffs.png")
+}
---
+++ src/simulation_functions.R
@@ -0,0 +1,88 @@
+#### SUBJECT SIMULATION
+
+cc_sim <- function(nsub, alpha, rho, omega, Ga){
+  
+  # empty array for simulated subject contributions 
+  c <- array(NA, c(nsub, 12))
+  
+  # empty array for trial-wise belief about others' contribution
+  Gb <- array(NA, c(nsub, 12))
+  
+  
+  for (s in 1:nsub){
+  
+  # Gb for first trial - SHOULD Gb BE CONTINUOUS OR DISCRETE?
+  Gb[s,1] <- extraDistr::rtpois(1,alpha[s],-1,3)
+  
+  # contribution for first trial
+  p <- rho*Gb[s,1]
+  c[s,1] <- extraDistr::rtpois(1,p,-1,3)
+  
+    # simulating subject behavior in remaining trials
+    for (t in 2:12) {
+      
+        # calculate Gb
+        Gb[s,t] <- ((1-omega[s])*(Gb[s, t-1]))+(omega[s]*(Ga[t-1]))
+        
+        # calculate poisson parameter
+        p <- rho*Gb[s,t]
+        
+        # sample contribution from poisson
+        c[s,t] <- extraDistr::rtpois(1,p,-1,3)
+    }
+  }
+  
+  data <- list(c = c, Gb = Gb)
+  
+  return(data)
+  
+}
+
+
+#### GROUP SIMULATION 
+
+group_cc_sim <- function(nsubjects, mu_alpha, mu_rho, mu_omega, Ga){ 
+  # nsubjects: number of subjects to simulate
+  # mu: the group mean of the given parameter
+  # lambda: the precision (I think) of that parameter
+  
+  # empty arrays to fill for all subjects: 
+  group_contributions <- array(NA, c(nsubjects, 12))
+  
+  # alpha
+  tau_alpha ~ dgamma(0.01,0.01)
+  sigma_alpha <- 1/sqrt(tau_alpha[i]) 
+  
+  rate_alpha <- ( mu_alpha + sqrt( mu_alpha^2 + 4*sigma_alpha^2 ) )/
+    (2*sigma_alpha^2) 
+  shape_alpha <- 1 + mu_alpha * rate_alpha
+  
+  # rho
+  sigma_rho ~ dunif(1,100)
+  
+  shape1_rho <- (mu_rho) * sigma_rho
+  shape2_rho <- (1 - mu_rho) * sigma_rho
+  
+  # omega
+  sigma_omega ~ dunif(1,100)
+  
+  shape1_omega <- (mu_omega) * sigma_omega
+  shape2_omega <- (1 - mu_omega) * sigma_omega
+  
+  # sampling the parameters based on the given mus and sigmas for each subject:
+  for (s in 1:nsubjects){
+    
+    # sample parameters
+    alpha ~ dgamma(shape_alpha,rate_alpha)T(0.001,)
+    rho ~ dbeta(shape1_rho,shape2_rho)T(0.001,0.999) 
+    omega ~ dbeta(shape1_omega,shape2_omega)T(0.001,0.999)
+    
+    # simulating the subject's behavior based on the sampled parameters
+    s_contributions <- cc_sim(alpha, rho, omega, Ga)
+    
+    # sample contribution from poisson
+    group_contributions[s,] <- s_contributions
+    }
+  
+  return(group_contributions)
+}    
