377865f360a5ec6c01a16dbd942d6edc4ed25226
daniblu <mr.daniblu@gmail.com>
2024-01-03 16:43:45 +0100

    debug, add ntrials, add group_cc_sim_no_reparam

--- src/simulation_functions.R
+++ src/simulation_functions.R
@@ -1,6 +1,7 @@
 library(extraDistr)
 library(IMIFA)
 library(cascsim)
+library(truncnorm)
 
 compute_Ga <- function(){
   # calculate Ga: trial-wise average contribution of other players
@@ -14,14 +15,19 @@ compute_Ga <- function(){
 
 #### SUBJECT SIMULATION
 
-cc_sim <- function(nsub, alpha, rho, omega, Ga){
+cc_sim <- function(nsub, alpha, rho, omega, Ga, ntrials=12){
   
   # empty array for simulated subject contributions 
-  c <- array(NA, c(nsub, 12))
+  c <- array(NA, c(nsub, ntrials))
   
   # empty array for trial-wise belief about others' contribution
-  Gb <- array(NA, c(nsub, 12))
+  Gb <- array(NA, c(nsub, ntrials))
   
+  # lengthen Ga if needed
+  if (ntrials != 12){
+    rest <- round(((ntrials/12 - floor(ntrials/12))*12), 1)
+    Ga <- c(rep(Ga, floor(ntrials/12)), Ga[1:rest])
+  }
   
   for (s in 1:nsub){
   
@@ -29,24 +35,24 @@ cc_sim <- function(nsub, alpha, rho, omega, Ga){
   Gb[s,1] <- extraDistr::rtpois(1,alpha[s],-1,3)
   
   # contribution for first trial
-  p <- rho*Gb[s,1]
+  p <- rho[s]*Gb[s,1]
   c[s,1] <- extraDistr::rtpois(1,p,-1,3)
   
     # simulating subject behavior in remaining trials
-    for (t in 2:12) {
+    for (t in 2:ntrials) {
       
         # calculate Gb
         Gb[s,t] <- ((1-omega[s])*(Gb[s, t-1]))+(omega[s]*(Ga[t-1]))
         
         # calculate poisson parameter
-        p <- rho*Gb[s,t]
+        p <- rho[s]*Gb[s,t]
         
         # sample contribution from poisson
         c[s,t] <- extraDistr::rtpois(1,p,-1,3)
     }
   }
   
-  data <- list(c = c, Gb = Gb)
+  data <- list(c = c, Gb = Gb, Ga = Ga)
   
   return(data)
   
@@ -55,12 +61,10 @@ cc_sim <- function(nsub, alpha, rho, omega, Ga){
 
 #### GROUP SIMULATION 
 
-group_cc_sim <- function(nsubjects, mu_alpha, mu_rho, mu_omega, Ga){ 
-  # nsubjects: number of subjects to simulate
-  # mu: the group mean of the given parameter
+group_cc_sim <- function(nsub, mu_alpha, mu_rho, mu_omega, Ga, ntrials=12){ 
   
   # empty arrays to fill for all subjects: 
-  group_contributions <- array(NA, c(nsubjects, 12))
+  group_contributions <- array(NA, c(nsub, ntrials))
   
   # alpha
   tau_alpha <- 1 # mean of dgamma(0.01,0.01)
@@ -81,21 +85,35 @@ group_cc_sim <- function(nsubjects, mu_alpha, mu_rho, mu_omega, Ga){
   
   shape1_omega <- (mu_omega) * sigma_omega
   shape2_omega <- (1 - mu_omega) * sigma_omega
+    
+  # sample parameters
+  alpha <- IMIFA::rltrgamma(nsub, shape_alpha, rate_alpha, trunc=0.001)
+  rho <- cascsim::rtbeta(nsub, shape1_rho, shape2_rho, min=0.001, max=0.999) 
+  omega <- cascsim::rtbeta(nsub, shape1_omega, shape2_omega, min=0.001, max=0.999)
   
-  # sampling the parameters based on the given mus and sigmas for each subject:
-  for (s in 1:nsubjects){
+  # simulating the subject's behavior based on the sampled parameters
+  sim <- cc_sim(nsub, alpha, rho, omega, Ga, ntrials=ntrials)
+  
+  # sample contribution from poisson
+  group_contributions <- sim$c
+  
+  return(group_contributions)
+}    
+
+
+
+group_cc_sim_no_reparam <- function(nsub, mu_alpha, mu_rho, mu_omega, Ga, ntrials=12){ 
     
-    # sample parameters
-    alpha <- IMIFA::rltrgamma(1, shape_alpha,rate_alpha, trunc=0.001)
-    rho <- cascsim::rtbeta(1, shape1_rho,shape2_rho, min=0.001, max=0.999) 
-    omega <- cascsim::rtbeta(1, shape1_omega,shape2_omega, min=0.001, max=0.999)
-    
-    # simulating the subject's behavior based on the sampled parameters
-    s_contributions <- cc_sim(alpha, rho, omega, Ga)
-    
-    # sample contribution from poisson
-    group_contributions[s,] <- s_contributions
-    }
+  # sample parameters
+  alpha <- truncnorm::rtruncnorm(nsub, a=0, b=10, mean=mu_alpha, sd=1)
+  rho <- truncnorm::rtruncnorm(nsub, a=0.001, b=0.999, mean=mu_rho, sd=.1)
+  omega <- truncnorm::rtruncnorm(nsub, a=0.001, b=0.999, mean=mu_omega, sd=.1)
+  
+  # simulating the subject's behavior based on the sampled parameters
+  sim <- cc_sim(nsub, alpha, rho, omega, Ga, ntrials=ntrials)
+  
+  # sample contribution from poisson
+  group_contributions <- sim$c
   
   return(group_contributions)
 }    
